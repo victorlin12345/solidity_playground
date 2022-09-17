5e5a9c380ddc8536ba10484f78e3cbe059baec42
VictorLin <victorlin@MB-VictorLin.local>
2022-09-17 14:51:53 +0800

    [week6] visibility and getter function modifier

---
+++ week6/1_visibility.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.7.0 < 0.9.0;
+
+contract Base {
+    /*
+    private: 同個contract內才能引用，繼承的也無法使用。
+    internal: 同個contract內可以使用，包含繼承的。
+    external: 同個contract內不可使用，繼承也不行，只能由第三方contract才能調用。
+    public: 開放的，上面的都可以調用。
+    */
+
+    // Private function can only be called
+    // - inside this contract
+    // Contracts that inherit this contract cannot call this function.
+    function privateFunc() private pure returns (string memory) {
+        return "private function called";
+    }
+
+    function testPrivateFunc() public pure returns (string memory) {
+        return privateFunc();
+    }
+
+    // Internal function can be called
+    // - inside this contract
+    // - inside contracts that inherit this contract
+    function internalFunc() internal pure returns (string memory) {
+        return "internal function called";
+    }
+
+    function testInternalFunc() public pure virtual returns (string memory) {
+        return internalFunc();
+    }
+
+    // Public functions can be called
+    // - inside this contract
+    // - inside contracts that inherit this contract
+    // - by other contracts and accounts
+    function publicFunc() public pure returns (string memory) {
+        return "public function called";
+    }
+
+    // External functions can only be called
+    // - by other contracts and accounts
+    function externalFunc() external pure returns (string memory) {
+        return "external function called";
+    }
+
+    // This function will not compile since we're trying to call
+    // an external function here.
+    // function testExternalFunc() public pure returns (string memory) {
+    //     return externalFunc();
+    // }
+
+    // State variables
+    string private privateVar = "my private variable";
+    string internal internalVar = "my internal variable";
+    string public publicVar = "my public variable";
+    // State variables cannot be external so this code won't compile.
+    // string external externalVar = "my external variable";
+}
+
+contract Child is Base {
+    // Inherited contracts do not have access to private functions
+    // and state variables.
+    // function testPrivateFunc() public pure returns (string memory) {
+    //     return privateFunc();
+    // }
+
+    // Internal function call be called inside child contracts.
+    function testInternalFunc() public pure override returns (string memory) {
+        return internalFunc();
+    }
+}
+
+contract Outside {
+    
+    address public currentActiveContractAddress;
+
+    function setActiveContractAddress(address _address) external {
+        currentActiveContractAddress = _address;
+    }
+
+    function callContractByFuncSignature() external returns (bool){
+        (bool success, ) = currentActiveContractAddress.call(abi.encodeWithSignature("externalFunc()"));
+        return success;
+    }
+}
+
+
+contract ViewAndPure {
+    uint public x = 1;
+
+    // Promise not to modify the state.
+    function addToX(uint y) public view returns (uint) {
+        return x + y;
+    }
+
+    // Promise not to modify or read from the state.
+    function add(uint i, uint j) public pure returns (uint) {
+        return i + j;
+    }
+}
